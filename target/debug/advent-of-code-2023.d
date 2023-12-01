7f9a86b42f5fdb0fe6de1eebbc7b34ca96db92b5
Milan Pavlik <pavlik.mil@gmail.com>
2023-12-01 09:59:52 +0100

    fix

--- target/debug/advent-of-code-2023.d
+++
@@ -1,1 +0,0 @@
-/Users/milanpavlik/workspace/advent-of-code-2023/target/debug/advent-of-code-2023: /Users/milanpavlik/workspace/advent-of-code-2023/src/main.rs
--- target/debug/deps/advent_of_code_2023-4fb8fb7015b7ff91.d
+++
@@ -1,5 +0,0 @@
-/Users/milanpavlik/workspace/advent-of-code-2023/target/debug/deps/advent_of_code_2023-4fb8fb7015b7ff91: src/main.rs
-
-/Users/milanpavlik/workspace/advent-of-code-2023/target/debug/deps/advent_of_code_2023-4fb8fb7015b7ff91.d: src/main.rs
-
-src/main.rs:
